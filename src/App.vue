eef3ceb9da6aebda7f44540d0ac92f17ffe6ad2c
Yuchen Liu <liuyuchenzh@163.com>
2020-09-02 15:16:03 +0800

    chore: simplify use case

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <Button>test</Button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import { Button } from "@clair/vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Button
   }
 };
 </script>
